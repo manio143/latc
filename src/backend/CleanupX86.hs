ae86b854afb684cd6d41424249815f4757149000
Marian Dziubiak <marian.dziubiak@gmail.com>
2019-01-24 19:43:50 +0100

    Fix X86 optimization issue

--- src/backend/CleanupX86.hs
+++ src/backend/CleanupX86.hs
@@ -13,15 +13,15 @@ clean (MOV a b : is) | a == b = clean is
 clean (MOV x y : MOV x' z : is) | x == x' && notDependent x z = clean (MOV x z : is)
 clean (MOV x y : MOV y' x' : is) | x == x' && y == y' = clean (MOV x y : is)
 
-clean (MOV bx x : SUB bx' y : MOV x' bx'' : is) | bx == bx' && bx == bx'' && x == x' = clean (SUB x y : is)
+clean (MOV bx x : SUB bx' y : MOV x' bx'' : is) | bx == bx' && bx == bx'' && x == x' && isTemp bx = clean (SUB x y : is)
 clean (MOV bx x : SUB bx' y : MOV z bx'' : is) | bx == bx' && bx == bx'' && z /= y && isTemp bx = clean (MOV z x : SUB z y : is)
 
-clean (MOV bx x : ADD bx' y : MOV x' bx'' : is) | bx == bx' && bx == bx'' && x == x' = clean (ADD x y : is)
-clean (MOV bx x : ADD bx' y : MOV y' bx'' : is) | bx == bx' && bx == bx'' && y == y' = clean (ADD y x : is)
+clean (MOV bx x : ADD bx' y : MOV x' bx'' : is) | bx == bx' && bx == bx'' && x == x' && isTemp bx = clean (ADD x y : is)
+clean (MOV bx x : ADD bx' y : MOV y' bx'' : is) | bx == bx' && bx == bx'' && y == y' && isTemp bx = clean (ADD y x : is)
 clean (MOV bx x : ADD bx' y : MOV z bx'' : is) | bx == bx' && bx == bx'' && z /= y && isTemp bx = clean (MOV z x : ADD z y : is)
 
-clean (MOV bx x : IMUL bx' y : MOV x' bx'' : is) | bx == bx' && bx == bx'' && x == x' = clean (IMUL x y : is)
-clean (MOV bx x : IMUL bx' y : MOV y' bx'' : is) | bx == bx' && bx == bx'' && y == y' = clean (IMUL y x : is)
+clean (MOV bx x : IMUL bx' y : MOV x' bx'' : is) | bx == bx' && bx == bx'' && x == x' && isTemp bx = clean (IMUL x y : is)
+clean (MOV bx x : IMUL bx' y : MOV y' bx'' : is) | bx == bx' && bx == bx'' && y == y' && isTemp bx = clean (IMUL y x : is)
 clean (MOV bx x : IMUL bx' y : MOV z bx'' : is) | bx == bx' && bx == bx'' && z /= y && isTemp bx = clean (MOV z x : IMUL z y : is)
 
 clean (MOV bx x : AND bx' y : MOV x' bx'' : is) | bx == bx' && bx == bx'' && x == x' = clean (AND x y : is)
